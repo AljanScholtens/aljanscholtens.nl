4b0c1c9eea933b1aaa6525a4b89f8b833f6f7538
Aljan Scholtens <aljan@scholtens.me>
2017-01-04 16:49:36 +0100

    Testing out image optimisers

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,19 @@ var cssnano = require('gulp-cssnano')
 var atImport = require('postcss-import')
 var lost = require('lost')
 var cssnext = require('postcss-cssnext')
+var gutil = require('gulp-util')
+
+// var imageOptim = require('gulp-imageoptim')
 var imagemin = require('gulp-imagemin')
+var jpegtran = require('imagemin-jpegtran')
+var jpegoptim = require('imagemin-jpegoptim')
+// var mozjpeg = require('imagemin-mozjpeg')
+
 var nunjucks = require('gulp-nunjucks')
 var browserSync = require('browser-sync').create()
 var modRewrite  = require('connect-modrewrite')
 // var smushit = require('gulp-smushit');
-var del = require('del');
+// var del = require('del');
 
 gulp.task('default', ['clean', 'serve'])
 
@@ -33,14 +40,14 @@ gulp.task('watch', function() {
   gulp.watch('src/**/*.html', ['html'])
   gulp.watch('src/assets/stylesheets/**/*.css', ['css'])
   gulp.watch('src/assets/images/**/*', ['images'])
-  gulp.watch('src/media/**/*', ['media'])
+  // gulp.watch('src/media/**/*', ['media'])
   gulp.watch('src/assets/scripts/**/*', ['scripts'])
 })
 
 gulp.task('build', ['html', 'css', 'scripts', 'images', 'media'])
 
 gulp.task('clean', function(cb) {
-  // return del('dist');
+//   return del('dist');
 });
 
 gulp.task('html', () =>
@@ -54,17 +61,32 @@ gulp.task('images', () =>
     .pipe(gulp.dest('dist/assets/images'))
 )
 
-gulp.task('media', () =>
-  gulp.src('src/media/**/*')
-  .pipe(imagemin({
-      optimizationLevel: 5,
+gulp.task('media', function() {
+  return gulp.src('src/media/**/*')
+
+    // .pipe(imagemin({
+    //   progressive: true,
+    //   use: [mozjpeg({ quality: '20' })]
+    // }))
+
+    // .pipe(imagemin({
+    //   progressive: true,
+    //   use: [jpegtran({ max: '20' })]
+    // }))
+
+    .pipe(imagemin({
       progressive: true,
-      interlaced: true,
-      multipass: true,
-      use: [imagemin.jpegtran({ max: 80 })],
+      use: [jpegoptim({ max: '20' })]
     }))
+
+    // .pipe(smushit())
+
+    // .pipe(imageOptim.optimize())
+
+    .on('error', gutil.log)
+
     .pipe(gulp.dest('dist/media'))
-)
+});
 
 gulp.task('scripts', () =>
   gulp.src('src/assets/scripts/*')
